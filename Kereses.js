7afe00bb6b0c6c09986949cb34999a492a8c0270
JustNutron <90186096+JustNutron@users.noreply.github.com>
2023-03-30 08:46:07 +0200

    Async fetch

--- Kereses.js
+++ Kereses.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
-import {Alert, Button, Pressable, StyleSheet, View, Text, FlatList, Image} from 'react-native';
-import { TextInput } from 'react-native-paper';
+import {TextInput, Pressable, StyleSheet, View, Text, FlatList, Image} from 'react-native';
 const IP = require('./ipcim');
-
-
 
 export default class ButtonBasics extends Component {
   constructor(props) {
@@ -13,53 +10,74 @@ export default class ButtonBasics extends Component {
       szo: "",
       dataSource2: [],
       dataSource3: [],
+      dataSource: [],
     };
   }
 
-  kattintas=()=> {
+  kattintas=async ()=>{
+  var datatomb=[]
+  var szamlalo=0
+  var bemenet={
+    bevitel1:this.state.szo
     
-    var bemenet={
-      bevitel1:this.state.szo
-     
-    }
-    // Alaplap keres
-    fetch(IP.ipcim+'keresalap', {
-      method: "POST",
-      body: JSON.stringify(bemenet),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-      this.setState({dataSource2: responseJson,
-      }, function(){
-
-      });
-
-    })
-    .catch((error) =>{
-    console.error(error);
-  });
-
-  //Processzor keres
-    fetch(IP.ipcim+'keresproci', {
-      method: "POST",
-      body: JSON.stringify(bemenet),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-        this.setState({dataSource3: responseJson,
-        }, function(){
-  
-      });
-  
-    })
-    .catch((error) =>{
-      console.error(error);
-    });
+   
   }
+  try {
+    
+    const response = await fetch(IP.ipcim+'keresproci',{
+      method: "POST",
+      body: JSON.stringify(bemenet),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    });
+    const json = await response.json();
+    console.log(json)
+
+    try {
+      const response2 = await fetch(IP.ipcim+'keresproci',{
+        method: "POST",
+        body: JSON.stringify(bemenet),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+      });
+      const json2 = await response2.json();
+      console.log(json)
+      for (const element of json) {
+        let elem = {
+          "elemid" : szamlalo,
+          "elemnev" : element.alaplap_neve,
+          "elemkep" : element.alaplap_kep,
+          "elemtipus" : "alaplap"
+        }
+        datatomb.push(elem)
+        szamlalo++
+      }
+      for (const element of json2) {
+        let elem = {
+          "elemid" : szamlalo,
+          "elemnev" : element.processzor_neve,
+          "elemkep" : element.processzor_kep,
+          "elemtipus" : "proci"
+        }
+        datatomb.push(elem)
+        szamlalo++
+      }
+      alert(JSON.stringify(datatomb))
+      this.setState({ dataSource: datatomb });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      
+    }
+
+
+   
+  } catch (error) {
+    console.log(error);
+  } finally {
+    
+  }
+}
+
+  
   
   render() {
     return (
@@ -67,70 +85,59 @@ export default class ButtonBasics extends Component {
       <View style={[styles.container,
         {
             flexDirection: "column"
-        }
-
-      ]}>
-        <View style={{flex: 1, backgroundColor: 'purple', borderRadius: 20}}>
+        }]}>
+        <View style={{flex: 1, backgroundColor: 'purple', borderRadius: 10, borderColor:'white', border: 10}}>
             <View style={styles.barview}>
             <TextInput
-              style={{height: 40, borderColor:"blue",borderWidth:1, margin:10, padding:10}}
+              style={{height: 40, margin:10, padding:10, backgroundColor: 'white'}}
               placeholder="Termék"
+              
+              
+              
               onChangeText={(szoveg) => this.setState({szo : szoveg})}
+              
               value={this.state.szo}
+              
       />       
                 
             </View>
             <View style={styles.buttonview}>
                 <Pressable onPress={()=>this.kattintas()}>
-                    <Text>Keresés</Text>
-
+                    <Text style={{alignSelf: 'center', marginTop: 10, paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10 ,backgroundColor: '#9D1DA7', borderRadius: 10, color: 'white'}} >Keresés</Text>
                 </Pressable>
+                
             </View>
         </View>
 
-            
+          
         <View style={{flex: 2, backgroundColor: 'black'}}>
         <FlatList
-            data={this.state.dataSource2}
-            keyExtractor={({ alaplap_id}, index) => alaplap_id}
+            data={this.state.dataSource}
+            keyExtractor={({ elemid }, index) => elemid}
             renderItem={({ item }) => (
 
               <View style={{marginBottom:30}}>
-              <Text style={styles.marka}>
-                {item.alaplap_marka}
-              </Text>
-              <Text style={styles.tipus}>
-                {item.alaplap_nev}
-              </Text>
-              <Image   source={{uri: IP.ipcim+item.alaplap_kep+'.png'}} style={styles.kep}   />          
-              </View>
-
               
-            )}
-          />
-          
-          <FlatList
-            data={this.state.dataSource3}
-            keyExtractor={({ processzor_id}, index) => processzor_id}
-            renderItem={({ item }) => (
-
-              <View style={{marginBottom:30}}>
-              <Text style={styles.marka}>
-                {item.processzor_marka}
+              <Text style={{color: 'white'}}>
+                {item.elemnev}
               </Text>
-              <Text style={styles.tipus}>
-                {item.processzor_nev}
-              </Text>
-              <Image   source={{uri: IP.ipcim+item.processzor_kep+'.png'}} style={styles.kep}   />          
+              
+              
+              <Image source={{uri: IP.ipcim+item.elemkep+'.png'}} style={styles.kep}/>    
               </View>
+
+          
             )}
           />
+
+          
         </View> 
         
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -171,6 +178,6 @@ const styles = StyleSheet.create({
     borderColor:'purple',
     borderWidth:5,
     marginLeft:10
-  }
-
+  },
 });
+
--- ipcim.js
+++ ipcim.js
@@ -1,3 +1,11 @@
 module.exports = {
-    ipcim: 'http://192.168.186.169:3000/'
+
+    ipcim:
+    //Blankáék nete: 
+    //'http://192.168.100.68:3000/'
+    //Mobilnetem:
+    'http://192.168.186.169:3000/'
+    //Otthoni:
+    //'http://:3000/'
+
 };
